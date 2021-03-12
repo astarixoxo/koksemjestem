f8315452a5768f4ec005825ad53e8f6853e6b6a1
56212 <56212@etu.he2b.be>
2021-03-12 18:16:11 +0100

    Ajout de la classe Tile (section 3.1)

---
+++ src/main/java/g56212/luckynumbers/model/Tile.java
@@ -0,0 +1,19 @@
+package g56212.luckynumbers.model;
+
+/**
+ *
+ * @author piotr
+ */
+public class Tile {
+
+    private int value;
+
+    public Tile(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    
+}
